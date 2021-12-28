98129c58263a9a43ee6aa2f4d6d906815f0f57ea
Mrrebrik <itibsoft@gmail.com>
2021-12-28 21:16:52 +0700

    TOAnimate; Animation static class of controller; SingletonMono to create GameObject

---
+++ Assets/Itibsoft/TOAnimate/Scenes/ExampleTOA.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Itibsoft.TOAnimate;
+
+public class ExampleTOA : MonoBehaviour
+{
+	[SerializeField] private Image _image;
+	private bool _isFadering = false;
+	public void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Space) && _isFadering == false)
+		{
+			_isFadering = true;
+			_image.TOAFade(_image.color.a == 1 ? 0 : 1, 1f, () => 
+			{ 
+				Debug.Log("Animation TOAFade to image complet!");
+				_isFadering = false;
+			});
+		}
+	}
+}
