eac4f606955981849b93617b109d0bdcd3ca46ac
Mrrebrik <itibsoft@gmail.com>
2021-12-28 21:25:54 +0700

    Example moving rect

--- Assets/Itibsoft/TOAnimate/Scenes/ExampleTOA.cs
+++ Assets/Itibsoft/TOAnimate/Scenes/ExampleTOA.cs
@@ -8,6 +8,9 @@ public class ExampleTOA : MonoBehaviour
 {
 	[SerializeField] private Image _image;
 	private bool _isFadering = false;
+	private bool _isMoving = false;
+	private bool _isLeft = false;
+	[SerializeField] private RectTransform[] _positions;
 	public void Update()
 	{
 		if (Input.GetKeyDown(KeyCode.Space) && _isFadering == false)
@@ -17,6 +20,16 @@ public class ExampleTOA : MonoBehaviour
 			{ 
 				Debug.Log("Animation TOAFade to image complet!");
 				_isFadering = false;
+			});
+		}
+		if (Input.GetKeyDown(KeyCode.A) && _isMoving == false)
+		{
+			_isMoving = true;
+			_image.rectTransform.TOAMove(_isLeft ? _positions[0].anchoredPosition : _positions[1].anchoredPosition, 5f, () =>
+			{
+				Debug.Log("Animation TOAMove to image complet!");
+				_isLeft = !_isLeft;
+				_isMoving = false;
 			});
 		}
 	}
